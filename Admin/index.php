36f177f8999d7a6774a777a0f8143af3a2c55f21
barastrong <145325312+barastrong@users.noreply.github.com>
2024-05-29 08:36:12 +0700

    first commit

--- Admin/index.php
+++ Admin/index.php
@@ -14,24 +14,18 @@ $username = $_SESSION['Username'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tugas akhir</title>
     <link rel="stylesheet" href="indexs.css">
-    <link
-      rel="icon"
-      href="https://static.republika.co.id/uploads/images/inpicture_slide/poster-solo-leveling-webcomic-yang-akan-diadaptasi-menjadi_220706173845-217.png"
-    />
-</head>
-<body>
-<header class="header">
+    <header class="header">
       <div class="header-logo">
         <img
-src="../logo.png"
-          style="width: 150px; height: 50%"
+          src="../logo.png"
+          style="width: 100px; height: 50%"
         />
       </div>
       <nav class="navbar">
         <a href="nextpage.php">Home</a>
-        <a href="nextpage.php">About</a>
+        <a href="#containers">About</a>
         <a href="admin/index.php">Admin Panel</a>
-        <a href="nextpage.php">Contact</a>
+        <a href="#social_icon">Contact</a>
         <a href="profile.php">Profile</a>
         <a href="../Logout.php">Logout</a>
       </nav>
@@ -39,6 +33,7 @@ src="../logo.png"
         <img
           src="https://static.republika.co.id/uploads/images/inpicture_slide/poster-solo-leveling-webcomic-yang-akan-diadaptasi-menjadi_220706173845-217.png"
           alt=""
+          class="img"
         />
         <p class="text"><?php echo htmlspecialchars($username); ?></p>
       </div>
